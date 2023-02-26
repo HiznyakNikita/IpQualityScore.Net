249c1af33f4aca93bb6e94400c1723d3638dd91f
Nikita <hiznyaknikita1996@gmail.com>
2023-02-26 12:33:08 +0200

    extract ipqualityscore route builder

---
+++ IpQualityScore.Net/IpQualityScore.Common/IpQualityScoreRouteBuilder.cs
@@ -0,0 +1,48 @@
+﻿using IpQualityScore.Common.Attributes;
+using IpQualityScore.Common.Exceptions;
+using IpQualityScore.Common.Extensions;
+using IpQualityScore.Common.Queries.Common;
+
+namespace IpQualityScore.Common
+{
+	internal static class IpQualityScoreRouteBuilder
+	{
+		public static async Task<string> Build<TQuery>(string baseUrl, string apiKey, TQuery query, string[] routeParts = null, string format = "json")
+			where TQuery : IpQualityScoreQuery
+		{
+			string requestUrl;
+			string route = GetRouteForQuery(query);
+			if (string.IsNullOrEmpty(route))
+				throw new IpQualityScoreException(null, $"Incorrect route for entity: {query.GetType().Name}");
+
+			if (string.IsNullOrEmpty(format))
+			{
+				requestUrl = routeParts != null && routeParts.Any()
+					? $"{baseUrl}/{apiKey}/{route}/{string.Join("/", routeParts)}?{await query.ToUrlEncodedString()}"
+					: $"{baseUrl}/{apiKey}/{route}?{await query.ToUrlEncodedString()}";
+			}
+			else if (format == "json")
+			{
+				requestUrl = routeParts != null && routeParts.Any()
+					? $"{baseUrl}/{format}/{route}/{apiKey}/{string.Join("/", routeParts)}?{await query.ToUrlEncodedString()}"
+					: $"{baseUrl}/{format}/{route}/{apiKey}?{await query.ToUrlEncodedString()}";
+			}
+			else
+				throw new IpQualityScoreException(null, $"Format {format} not supported");
+
+			if (string.IsNullOrEmpty(requestUrl))
+				throw new IpQualityScoreException(null, $"Incorrect request url for entity: {query.GetType().Name}");
+
+			return requestUrl;
+		}
+		private static string GetRouteForQuery<TQuery>(TQuery query)
+			where TQuery : IpQualityScoreQuery
+		{
+			var attribute = query.GetType().GetCustomAttributes(false)
+				.FirstOrDefault(a => a is ApiRouteAttribute);
+			var route = (attribute as ApiRouteAttribute)?.Route;
+
+			return route;
+		}
+	}
+}
