f7620d461140c903e69d9a960125565e3d22fade
Nikita <hiznyaknikita1996@gmail.com>
2023-03-04 16:57:20 +0200

    extracted IpQualityScoreRouteBuilder and add tests

--- IpQualityScore.Net/IpQualityScore.Common/IpQualityScoreRouteBuilder.cs
+++ IpQualityScore.Net/IpQualityScore.Common/IpQualityScoreRouteBuilder.cs
@@ -7,6 +7,7 @@ namespace IpQualityScore.Common
 {
 	internal static class IpQualityScoreRouteBuilder
 	{
+		private static readonly string[] _allowedFormats = new[] { "json" };
 		public static async Task<string> Build<TQuery>(string baseUrl, string apiKey, TQuery query, string[] routeParts = null, string format = "json")
 			where TQuery : IpQualityScoreQuery
 		{
@@ -15,17 +16,21 @@ namespace IpQualityScore.Common
 			if (string.IsNullOrEmpty(route))
 				throw new IpQualityScoreException(null, $"Incorrect route for entity: {query.GetType().Name}");
 
+			routeParts = routeParts != null ? routeParts.Where(r => !string.IsNullOrEmpty(r)).ToArray() : null;
+			var urlEncodedQuery = await query.ToUrlEncodedString();
+			var queryString = string.IsNullOrEmpty(urlEncodedQuery) ? "" : $"?{urlEncodedQuery}";
+
 			if (string.IsNullOrEmpty(format))
 			{
 				requestUrl = routeParts != null && routeParts.Any()
-					? $"{baseUrl}/{apiKey}/{route}/{string.Join("/", routeParts)}?{await query.ToUrlEncodedString()}"
-					: $"{baseUrl}/{apiKey}/{route}?{await query.ToUrlEncodedString()}";
+					? $"{baseUrl}/{apiKey}/{route}/{string.Join("/", routeParts)}{queryString}"
+					: $"{baseUrl}/{apiKey}/{route}{queryString}";
 			}
-			else if (format == "json")
+			else if (_allowedFormats.Contains(format))
 			{
 				requestUrl = routeParts != null && routeParts.Any()
-					? $"{baseUrl}/{format}/{route}/{apiKey}/{string.Join("/", routeParts)}?{await query.ToUrlEncodedString()}"
-					: $"{baseUrl}/{format}/{route}/{apiKey}?{await query.ToUrlEncodedString()}";
+					? $"{baseUrl}/{format}/{route}/{apiKey}/{string.Join("/", routeParts)}{queryString}"
+					: $"{baseUrl}/{format}/{route}/{apiKey}{queryString}";
 			}
 			else
 				throw new IpQualityScoreException(null, $"Format {format} not supported");
