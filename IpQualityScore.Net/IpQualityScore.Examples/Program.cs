f90580281d5d2cc2329cb4c87e4120aceffea2e4
Nikita <hiznyaknikita1996@gmail.com>
2023-03-19 14:19:07 +0200

    fix issue with typed request list api

--- IpQualityScore.Net/IpQualityScore.Examples/Program.cs
+++ IpQualityScore.Net/IpQualityScore.Examples/Program.cs
@@ -3,7 +3,7 @@ using IpQualityScore.Net.Requests;
 using IpQualityScore.Net.Results;
 using Microsoft.Extensions.DependencyInjection;
 
-var apiKey = "bKaxzWxXop1bJ0bRM6KyQZ1SCpKKJqeD";
+var apiKey = "";
 var serviceProvider = new ServiceCollection()
 			.AddIpQualityScore(apiKey)
 			.BuildServiceProvider();
@@ -15,56 +15,56 @@ try
 {
 	if (IpQualityScore is not null)
 	{
-		//var emailValidationRequest = new EmailValidationRequest()
-		//{
-		//	Email = "",
-		//	Fast = true,
-		//	AbuseStrictness = 2,
-		//	Strictness = 2,
-		//	SuggestDomain = true,
-		//	Timeout = 30
-		//};
-		//var emailValidationResult = await IpQualityScore.Email.Validate(emailValidationRequest);
-		//Console.WriteLine($"Is email {emailValidationRequest.Email} valid: {emailValidationResult.Valid}");
+		var emailValidationRequest = new EmailValidationRequest()
+		{
+			Email = "",
+			Fast = true,
+			AbuseStrictness = 2,
+			Strictness = 2,
+			SuggestDomain = true,
+			Timeout = 30
+		};
+		var emailValidationResult = await IpQualityScore.Email.Validate(emailValidationRequest);
+		Console.WriteLine($"Is email {emailValidationRequest.Email} valid: {emailValidationResult.Valid}");
 
-		//var resultReportRequest = new ReportRequest() { Email = "bad_email@example.com" };
-		//var resultReport = await IpQualityScore.Reports.Send(resultReportRequest);
-		//Console.WriteLine($"{resultReport.Success} {resultReport.Message} {resultReport.RequestId}");
+		var resultReportRequest = new ReportRequest() { Email = "bad_email@example.com" };
+		var resultReport = await IpQualityScore.Reports.Send(resultReportRequest);
+		Console.WriteLine($"{resultReport.Success} {resultReport.Message} {resultReport.RequestId}");
 
-		//var transactionValidationRequest = new TransactionRiskScoringRequest()
-		//{
-		//	IpAddress = "",
-		//	Strictness = 1,
-		//	BillingEmail = "",
-		//	BillingCountry = ""
+		var transactionValidationRequest = new TransactionRiskScoringRequest()
+		{
+			IpAddress = "",
+			Strictness = 1,
+			BillingEmail = "",
+			BillingCountry = ""
 
-		//};
-		//var transactionValidationResult = await IpQualityScore.Transaction.Validate(transactionValidationRequest);
-		//Console.WriteLine($"Risk factors: {string.Join(",", transactionValidationResult.TransactionDetails.RiskFactors)}");
+		};
+		var transactionValidationResult = await IpQualityScore.Transaction.Validate(transactionValidationRequest);
+		Console.WriteLine($"Risk factors: {string.Join(",", transactionValidationResult.TransactionDetails.RiskFactors)}");
 
-		//var urlValidationRequest = new UrlValidationRequest()
-		//{
-		//	Url = "",
-		//	Fast = true,
-		//	Strictness = 2,
-		//	Timeout = 30
-		//};
-		//var urlValidationResult = await IpQualityScore.Url.Validate(urlValidationRequest);
-		//Console.WriteLine($"Is url {urlValidationRequest.Url} safe: {!urlValidationResult.Unsafe}");
+		var urlValidationRequest = new UrlValidationRequest()
+		{
+			Url = "",
+			Fast = true,
+			Strictness = 2,
+			Timeout = 30
+		};
+		var urlValidationResult = await IpQualityScore.Url.Validate(urlValidationRequest);
+		Console.WriteLine($"Is url {urlValidationRequest.Url} safe: {!urlValidationResult.Unsafe}");
 
-		//var statsRequest = new StatsRequest()
-		//{
-		//	Country = "",
-		//	EndDate = new DateOnly(2023, 2, 1),
-		//	StartDate = new DateOnly(2023, 2, 20),
-		//	CustomVariables = new Dictionary<string, string>()
-		//	{
-		//		{"UserId", "123123"},
-		//		{"myvariable", "22222" }
-		//	}
-		//};
-		//var stats = await IpQualityScore.Stats.Get(statsRequest);
-		//Console.WriteLine($"Stats fraud average: {stats.FraudAverage}");
+		var statsRequest = new StatsRequest()
+		{
+			Country = "",
+			EndDate = new DateOnly(2023, 2, 1),
+			StartDate = new DateOnly(2023, 2, 20),
+			CustomVariables = new Dictionary<string, string>()
+			{
+				{"UserId", "123123"},
+				{"myvariable", "22222" }
+			}
+		};
+		var stats = await IpQualityScore.Stats.Get(statsRequest);
+		Console.WriteLine($"Stats fraud average: {stats.FraudAverage}");
 
 		var requestApiRequest = new IpQualityScoreRequestApiRequest()
 		{
@@ -75,15 +75,15 @@ try
 		var requestsResult = await IpQualityScore.Requests.Get<EmailValidationResult>(requestApiRequest);
 		Console.WriteLine($"Requests: {string.Join(";", requestsResult.Requests.Select(r => r.RequestId))}");
 
-//		var creditRequest = new CreditRequest()
-//		{
-//		};
-//		var creditResult = await IpQualityScore.Credits.Get(creditRequest);
-//		Console.WriteLine(@$"Credits: {creditResult.Credits};
-//Usage: {creditResult.Usage}; 
-//EmailUsage: {creditResult.EmailUsage}; 
-//ProxyUsage: {creditResult.ProxyUsage}; 
-//FingerprintUsage: {creditResult.FingerprintUsage}");
+		var creditRequest = new CreditRequest()
+		{
+		};
+		var creditResult = await IpQualityScore.Credits.Get(creditRequest);
+		Console.WriteLine(@$"Credits: {creditResult.Credits};
+Usage: {creditResult.Usage}; 
+EmailUsage: {creditResult.EmailUsage}; 
+ProxyUsage: {creditResult.ProxyUsage}; 
+FingerprintUsage: {creditResult.FingerprintUsage}");
 	}
 
 }
